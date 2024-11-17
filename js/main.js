2dda5fb6bf354b9b14efe68454d8b020f95f109e
nasrashvili <lukanasrashvili1@gmail.com>
2024-11-17 14:33:06 -0500

    add js and html

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const erray = [30, 35, 45, 50, 55];
+console.log(30 + 35 + 45 + 50 + 55);
+const erray1 = 215;
+console.log(erray1 / 5);
+
+const mymarket = {
+  title: "electrical appliances",
+  author: "jimsher",
+  yearPublished: 1121,
+};
+mymarket.title = "electrical appliances by ";
+mymarket.author = " jimsher was published in ";
+console.log(mymarket.title + mymarket.author + mymarket.yearPublished);
+
+mymarket.yearPublished = 2024;
+console.log(mymarket);
+
+const lastarray = {
+  name: ["luka"],
+  age: [24],
+  score: [2028],
+};
+console.log(lastarray);
